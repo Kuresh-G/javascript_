b0f588c2abb5380dda1830b7521ee0499efe5310
Kuresh-G <kureshgarbada@gmail.com>
2025-01-05 06:11:10 +0000

    conversion of datatype

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,24 @@
+let score= false
+console.log(typeof score);
+let valueinNumber = Number(score);
+console.log(typeof valueinNumber);
+console.log(valueinNumber)
+
+/*
+33 -> 33
+33abc -> Nan
+true -> 1
+false -> 0
+null -> 0
+*/
+let temp="kk"
+let bool_temp = Boolean(temp)
+console.log(bool_temp)
+/*
+empty string , 0   -> false
+any character or any number -> true
+*/
+
+let num = 32
+let str = String(num)
+console.log(typeof str)
