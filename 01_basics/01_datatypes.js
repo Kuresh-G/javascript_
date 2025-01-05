aad879bf43bdb623d2c9c6ab2a67f4d897c248b2
Kuresh-G <kureshgarbada@gmail.com>
2025-01-05 05:48:20 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,33 @@
+ 
+// alert(3+3)   it is not used this way as we are using node js not browser
+let name ="kuresh"  // string
+let age=20  //integer
+let isloggedin=true  // boolean
+/* 
+primary datatypes
+
+
+ number -> 2 to power 53
+bigint
+string -> ""  
+boolean -> true/false
+null->standalone value  //  absensce of object.
+undefined -> value not assigned     // absence of value.
+symbol -> unique
+
+objects
+*/
+console.log(typeof name)
+console.log(typeof null)   // null  is an object type
+console.log(typeof undefined)    // undefine is undefine type
+
+const foo = 42; // foo is a number
+const result = foo + "1"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
+console.log(result); // 421
+
+console.log(42 / +0); // Infinity
+console.log(42 / -0); // -Infinity
+
+
+
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const number=1234
+let name = "kuresh"     // let and var are used for variables  and let is used nowaday.
+var surname ="garbada"
+city="dahod"
+ console.table([number,name,surname,city])
+// number = 4567      not changed
+name="hatim"
+surname="garba"
+city="baroda"
+console.table([number,name,surname,city])
+let temp      // if no value is there then undefined will be shown.
+console.log(temp);
